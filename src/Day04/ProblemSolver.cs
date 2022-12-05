c5d5418c1000ab54145af0cba2310549e7c9bebf
Jeremy Capello <me@lethargicdeveloper.com>
2022-12-04 20:55:01 -0600

    added benchmarks

---
+++ src/Day04/ProblemSolver.cs
@@ -0,0 +1,75 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text.RegularExpressions;
+
+namespace Day04;
+
+[MinColumn, MaxColumn, MemoryDiagnoser]
+public partial class ProblemSolver
+{
+    int[] ToRange(string list)
+        => list.Split("-").Select(int.Parse).ToArray();
+
+    bool InRange(int[] r1, int[] r2)
+        => r1[0] <= r2[0] && r1[1] >= r2[1];
+
+    bool Overlap(int[] r1, int[] r2)
+        => r1[0] <= r2[1] && r1[1] >= r2[0];
+
+    bool Overlap(int[] r)
+        => r[0] <= r[3] && r[1] >= r[2];
+
+    [GeneratedRegex(@"\d+", RegexOptions.Compiled, "en-US")]
+    private static partial Regex GeneratedRegex();
+
+    public int SolvePart1()
+        => PuzzleInput.Input001
+            .Split("\r\n")
+            .Select(_ => _.Split(",").Select(ToRange).ToArray())
+            .Where(_ => InRange(_[0], _[1]) || InRange(_[1], _[0]))
+            .Count();
+
+    [Benchmark]
+    public int SolveWithStringSplit()
+        => PuzzleInput.Input001
+            .Split("\r\n")
+            .Select(_ => _.Split(",")
+                .Select(ToRange)
+                .ToArray())
+            .Where(_ => Overlap(_[0], _[1]))
+            .Count();
+
+    [Benchmark]
+    public int SolveWithRegex()
+        => PuzzleInput.Input001
+            .Split("\r\n")
+            .Select(_ => Regex.Matches(_, @"\d+")
+                .Select(x => int.Parse(x.Value))
+                .ToArray())
+            .Where(Overlap)
+            .Count();
+
+    [Benchmark]
+    public int SolveWithCompiledRegex()
+    {
+        var regex = new Regex(@"\d+", RegexOptions.Compiled);
+        return PuzzleInput.Input001
+            .Split("\r\n")
+            .Select(_ => regex.Matches(_)
+                .Select(x => int.Parse(x.Value))
+                .ToArray())
+            .Where(Overlap)
+            .Count();
+    }
+
+    [Benchmark]
+    public int SolveWithGeneratedRegex()
+    {
+        return PuzzleInput.Input001
+            .Split("\r\n")
+            .Select(_ => GeneratedRegex().Matches(_)
+                .Select(x => int.Parse(x.Value))
+                .ToArray())
+            .Where(Overlap)
+            .Count();
+    }
+}
--- src/Day04/Program.cs
+++ src/Day04/Program.cs
@@ -1,24 +1,10 @@
-﻿using Day04;
+﻿using BenchmarkDotNet.Running;
+using Day04;
 
-int[] ToRange(string list)
-    => list.Split("-").Select(int.Parse).ToArray();
-
-bool InRange(int[] r1, int[] r2)
-    => r1[0] <= r2[0] && r1[1] >= r2[1];
-
-PuzzleInput.Input001
-    .Split("\r\n")
-    .Select(_ => _.Split(",").Select(ToRange).ToArray())
-    .Where(_ => InRange(_[0], _[1]) || InRange(_[1], _[0]))
-    .Count()
-    .Log("Part 1");
-
-bool Overlap(int[] r1, int[] r2)
-    => r1[0] <= r2[1] && r1[1] >= r2[0];
-
-PuzzleInput.Input001
-    .Split("\r\n")
-    .Select(_ => _.Split(",").Select(ToRange).ToArray())
-    .Where(_ => Overlap(_[0], _[1]) || Overlap(_[1], _[0]))
-    .Count()
-    .Log("Part 2");
+#if DEBUG
+    new ProblemSolver().SolveWithStringSplit().Log("Part 2 - String Split");
+    new ProblemSolver().SolveWithRegex().Log("Part 2 - Regex");
+    new ProblemSolver().SolveWithCompiledRegex().Log("Part 2 - Compiled Regex");
+#else
+    BenchmarkRunner.Run<ProblemSolver>();
+#endif

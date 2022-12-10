6112f8fa3c9fd5221e23c06ebb259b8d2d22a6ca
Jeremy Capello <me@lethargicdeveloper.com>
2022-12-10 14:34:23 -0600

    refactor

--- src/Day10/PuzzleSolver.cs
+++ src/Day10/PuzzleSolver.cs
@@ -1,6 +1,8 @@
 ﻿using BenchmarkDotNet.Attributes;
 using Microsoft.Diagnostics.Tracing.Parsers.FrameworkEventSource;
 using Microsoft.Diagnostics.Tracing.Parsers.JScript;
+using Microsoft.Extensions.Primitives;
+using System.Text;
 
 [MinColumn, MaxColumn, MemoryDiagnoser]
 public partial class PuzzleSolver
@@ -40,51 +42,35 @@ public partial class PuzzleSolver
         return strengths.Sum();
     }
 
+    void NextCycle(StringBuilder crt, int pos, ref int cycles)
+    {
+        cycles++;
+        int x = (cycles - 1) % 40;
+
+        crt.Append(x >= pos - 1 && x <= pos + 1 ? "#" : ".");
+        if (x == 39) crt.AppendLine();
+    }
+
     public string SolvePart2()
     {
-        var crt = new char[6][];
-        for (int y = 0; y < 6; ++y)
-        {
-            crt[y] = new char[40];
-            for (int x = 0; x < 39; ++x)
-                crt[y][x] = '.';
-        }
+        var crt = new StringBuilder("\r\n");
 
-        var h = 1;
-        var v = 0;
+        var pos = 1;
         var cycles = 0;
 
         foreach (var line in this.input)
         {
-            var cmd = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var cmd = line.Split(' ');
 
-            cycles++;
-            if (cycles > 40)
-            {
-                v++;
-                cycles = 1;
-            }
-            if (cycles - 1 >= h - 1 && cycles - 1 <= h + 1)
-            {
-                crt[v][cycles - 1] = '#';
-            }
+            NextCycle(crt, pos, ref cycles);
             
             if (cmd[0] == "addx")
             {
-                cycles++;
-                if (cycles > 40)
-                {
-                    v++;
-                    cycles = 1;
-                }
-                if (cycles - 1 >= h - 1 && cycles - 1 <= h + 1)
-                {
-                    crt[v][cycles - 1] = '#';
-                }
-                h += int.Parse(cmd[1]);
+                NextCycle(crt, pos, ref cycles);
+                pos += int.Parse(cmd[1]);
             }
         }
 
-        return "\r\n" + string.Join("\r\n", crt.Select(_ => new string(_)));
+        return crt.ToString();
     }
 }

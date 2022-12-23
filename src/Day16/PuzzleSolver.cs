6df1322d589a5bee90ca0e65f348afda5aada0c0
Jeremy Capello <me@lethargicdeveloper.com>
2022-12-23 15:11:28 -0600

    day 16... blah

--- src/Day16/PuzzleSolver.cs
+++ src/Day16/PuzzleSolver.cs
@@ -1,16 +1,15 @@
 ﻿using QuickGraph;
 using System.Diagnostics;
-using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 
-public partial class PuzzleSolver2
+public partial class PuzzleSolver
 {
     readonly List<string> input;
     readonly DelegateVertexAndEdgeListGraph<Valve, EquatableEdge<Valve>> graph;
 
-    public PuzzleSolver2()
+    public PuzzleSolver()
     {
-        this.input = File.ReadLines("InputExample.txt").ToList();
+        this.input = File.ReadLines("Input001.txt").ToList();
 
         graph = CreateGraph(ParseInput());
     }
@@ -29,16 +28,20 @@ public partial class PuzzleSolver2
         // get all possible paths that hit all important nodes
         var importantNodes = graph.Vertices.Where(ImportantNode).ToList();
         
-        var pathParts = new List<List<List<Valve>>>();
         foreach (var pair in pairs.Where(_ => _[0].Name == "AA"))
         {
             var path = GetPaths(new[] { pair }.ToList(), pairs, importantNodes);
-            pathParts.Add(path);
+            
+            long pressure = 0;
+            foreach (var p in path)
+            {
+                if (p > pressure)
+                {
+                    pressure = p;
+                    Console.WriteLine(pressure);
+                }
+            }
         }
-
-        var paths = pathParts.SelectMany(_ => _).ToList();
-
-        var pressure = GetPressure(paths);
 
         return 0;
     }
@@ -48,7 +51,122 @@ public partial class PuzzleSolver2
         return 0;
     }
 
-    List<List<Valve>> GetPaths(List<List<Valve>> segments, List<List<Valve>> shortestPaths, List<Valve> importantNodes)
+    long GetPressure(List<List<Valve>> paths)
+    {
+        var pressures = new List<long>();
+        foreach (var path in paths)
+            pressures.Add(GetPressure(path));
+
+        return pressures.Max();
+    }
+
+    long GetPressure(List<Valve> path)
+    {
+        //var answer = new List<Valve>
+        //{
+        //    new Valve {Name = "AA"},
+        //    new Valve {Name = "DD"},
+        //    new Valve {Name = "CC"},
+        //    new Valve {Name = "BB"},
+        //    new Valve {Name = "AA"},
+        //    new Valve {Name = "II"},
+        //    new Valve {Name = "JJ"},
+        //    new Valve {Name = "II"},
+        //    new Valve {Name = "AA"},
+        //    new Valve {Name = "DD"},
+        //    new Valve {Name = "EE"},
+        //    new Valve {Name = "FF"},
+        //    new Valve {Name = "GG"},
+        //    new Valve {Name = "HH"},
+        //    new Valve {Name = "GG"},
+        //    new Valve {Name = "FF"},
+        //    new Valve {Name = "EE"},
+        //    new Valve {Name = "DD"},
+        //    new Valve {Name = "CC"},
+        //};
+
+        var pressures = new List<(int ValveIndex, long Pressure, List<(Valve Valve, bool Open)> OpenValves)>();
+
+        int time = 0;
+        while (true)
+        {
+            // increase time
+            time++;
+
+            // we're done if all path lines time > 30
+            if (time > 30) break;
+
+            // check pressure
+            for (int i = 0; i < pressures.Count; ++i)
+            {
+                var (_, pressure, openValves) = pressures[i];
+                
+                pressure += openValves.Where(_ => _.Open).Select(_ => _.Valve.Rate).Sum();
+                pressures[i] = pressures[i] with
+                {
+                    Pressure = pressure,
+                };
+            }
+
+            // get combinations for whether to open the valve or not
+            if (pressures.Count == 0)
+            {
+                pressures.Add((0, 0, new List<(Valve, bool)> { (path[0], false) }));
+            } 
+            else
+            {
+                var updatedPressures = new List<(int ValveIndex, long Pressure, List<(Valve Valve, bool Open)> OpenValves)>();
+                for (int i = 0; i < pressures.Count; ++i)
+                {
+                    var (valveIndex, pressure, openValves) = pressures[i];
+                    var valve = path[valveIndex];
+
+                    var openClosed = new List<(Valve valve, bool open)>
+                    {
+                        (valve, false)
+                    };
+
+                    if (valve.Rate > 0 && !openValves.Contains((valve, true)))
+                    {
+                        openClosed.Add((valve, true));
+                    }
+
+                    updatedPressures.AddRange((
+                        from v in openClosed
+                        from p in new[] { pressures[i] }
+                        select p with
+                        {
+                            OpenValves = p.OpenValves.Concat(new[] { v }).ToList()
+                        }
+                    ).ToList());
+                }
+                pressures = updatedPressures;
+            }
+
+            // check whether to move or open a valve
+            for (int i = 0; i < pressures.Count; ++i)
+            {
+                var (valveIndex, pressure, openValves) = pressures[i];
+
+                if (!openValves[^1].Open)
+                {
+                    if (valveIndex < path.Count - 1)
+                        valveIndex++;
+                }
+
+                pressures[i] = pressures[i] with
+                {
+                    ValveIndex = valveIndex
+                };
+            }
+        }
+
+        return pressures
+            .Select(_ => _.Pressure)
+            .Max();
+    }
+
+    IEnumerable<long> GetPaths(List<List<Valve>> segments, List<List<Valve>> shortestPaths, List<Valve> importantNodes)
     {
         var paths = (
             from segment in segments
@@ -62,6 +180,16 @@ public partial class PuzzleSolver2
             var newPathSegments = new List<List<List<Valve>>>();
             foreach (var path in paths)
             {
+                if (importantNodes.All(_ => path.SelectMany(v => v).Contains(_)))
+                {
+                    var p = RemoveDuplicateNodes(paths);
+                    foreach (var p1 in p)
+                    {
+                        var pressure = GetPressure(p1);
+                        yield return pressure;
+                    }
+                }
+
                 newPathSegments.Add((
                     from segment in new[] { path[^1][^1] }
                     from shortestPath in shortestPaths
@@ -92,8 +220,6 @@ public partial class PuzzleSolver2
 
             paths = newPath;
         }
-
-        return RemoveDuplicateNodes(paths);
     }
 
     List<List<Valve>> RemoveDuplicateNodes(List<List<List<Valve>>> paths)
@@ -118,7 +244,20 @@ public partial class PuzzleSolver2
         return p2;
     }
 
-    IEnumerable<Valve> Flatten(IEnumerable<List<Valve>> paths) => paths.SelectMany(_ => _);
+    List<List<Valve>> RemoveDuplicatePaths(List<List<Valve>> paths)
+    {
+        var p = new List<List<Valve>>();
+        
+        for (int i = 0; i < paths.Count; ++i)
+        {
+            if (!p.Any(_ => Enumerable.SequenceEqual(_, paths[i])))
+            {
+                p.Add(paths[i]);
+            }
+        }
+
+        return p;
+    }
 
     bool StartNode(Valve valve) => valve.Name == "AA";
     bool ImportantNode(Valve valve) => valve.Rate > 0;
@@ -216,7 +355,6 @@ public partial class PuzzleSolver2
     {
         public string Name { get; set; } = "";
         public int Rate { get; set; }
-        public bool Open { get; set; }
         public List<Valve> Tunnels { get; set; } = new();
 
         public override bool Equals(object? obj)
